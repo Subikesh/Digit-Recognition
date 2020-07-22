496187a80c32b84149d70ac4a117245a27f28071
Subikesh <subikeshps@gmail.com>
2020-07-22 09:05:40 +0530

    Adding SECRET_KEY

--- digits/settings.py
+++ digits/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0zot@9tg#ymf%h-z6y-y_v#=f=^4ar1i*xv71q=l^4e$df&ydn'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
 ALLOWED_HOSTS = ['subikeshdigits.herokuapp.com']
 
